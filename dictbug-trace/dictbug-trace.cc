29321102ecb61969c5e743ae587d63d034296b3f
Patrick Gaskin <patrick@pgaskin.net>
2020-05-15 12:42:15 -0400

    dictbug-trace: Added another function

--- dictbug-trace/dictbug-trace.cc
+++ dictbug-trace/dictbug-trace.cc
@@ -15,7 +15,7 @@
 #include <dlfcn.h>
 #include <syslog.h>
 
-#define LOG(format, ...) syslog(LOG_DEBUG, "(dictbug-trace) " format, __VA_ARGS__)
+#define LOG(format, ...) syslog(LOG_DEBUG, "(dictbug-trace) " format, ##__VA_ARGS__)
 #define SYM(var, sym) reinterpret_cast<void*&>(var) = dlsym(RTLD_NEXT, sym)
 #define constructor __attribute__((constructor))
 
@@ -23,12 +23,14 @@ static void(*DictionaryWebview_setHtml_orig)(void*, QString const&, QUrl const&)
 static QNetworkReply*(*QNetworkAccessManager_get_orig)(void*, QNetworkRequest const&);
 static void*(*DictionaryParser_getFile_orig)(void*, void*, QString const&);
 static void*(*QFileInfo_QFileInfo_orig)(void*, QString const&);
+static void*(*DictionaryFileManager_getDictionarySizeOnDisk)(void*, void*, void*);
 
 constructor static void init() {
     SYM(DictionaryWebview_setHtml_orig, "_ZN17DictionaryWebview7setHtmlERK7QStringRK4QUrl");
     SYM(QNetworkAccessManager_get_orig, "_ZN21QNetworkAccessManager3getERK15QNetworkRequest");
     SYM(DictionaryParser_getFile_orig, "_ZN16DictionaryParser7getFileEP7ZipFileRK7QString");
     SYM(QFileInfo_QFileInfo_orig, "_ZN9QFileInfoC1ERK7QString");
+    SYM(DictionaryFileManager_getDictionarySizeOnDisk, "_ZN21DictionaryFileManager23getDictionarySizeOnDiskERK6DeviceRK10Dictionary");
 }
 
 extern "C" void _ZN17DictionaryWebview7setHtmlERK7QStringRK4QUrl(void* _this, QString const& path, QUrl const& url) {
@@ -55,4 +57,9 @@ extern "C" void* _ZN9QFileInfoC1ERK7QString(void* _this, QString const& filename
     if (v)
         LOG("QFileInfo::size() = %lld", reinterpret_cast<QFileInfo*>(_this)->size());
     return res;
+}
+
+extern "C" void* _ZN21DictionaryFileManager23getDictionarySizeOnDiskERK6DeviceRK10Dictionary(void* _this, void* device, void* dictionary) {
+    LOG("DictionaryFileManager::getDictionarySizeOnDisk (note: this is one of the QFileInfo::size callers)");
+    return DictionaryFileManager_getDictionarySizeOnDisk(_this, device, dictionary);
 }
